0981e0cf25af518823ca036a796008b60039e3a4
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-10-28 10:13:40 -0400

    On branch 3-build-painter-class  Your branch is up to date with 'origin/3-build-painter-class'.
     Changes to be committed:
    	modified:   makefile
    	modified:   pong/render.py
    	new file:   tests/test_Painter.py
    - set up painter unit test skeleton

--- pong/render.py
+++ pong/render.py
@@ -2,7 +2,8 @@ from mcpi.minecraft import Minecraft
 from mcpi import vec3
 import numpy as np
 
-      
+
+ 
 class Color:
     """
     The Color class uses a Wool block (id=35), to represent the colors of the pixels.  According to the reference: 
@@ -56,7 +57,125 @@ class Color:
         
         """
         return Color.__pixel_color[color_num]
+
+
+class PixelArray:
+    """
+    This class creates an manages a raster of pixels.  Each pixel spot holds an encoded color as shown below:
+
+    0=black wool
+    1=green wool
+    2=blue wool
+    3=red wool
+    4=white wool  
+    5=Magenta wool
+    6=Brown wool
+    7=Purple wool
+    8=Cyan wool
+    9=Transparent/do not draw
+    10=Grey wool
+    11=Pink wool
+    12=Lime wool
+    13=Yellow wool
+    14=Light Blue wool
+    15=Orange wool
+    16=Light Grey wool
+    """
+
+
+    def __init__(self, my_array, transpose=True):
+        """
+        Creates a new PixelArray from an existing array
+
+        my_array:   (numpy.ndarray)     1D or 2D array representing the pixelspace
+        transpose:  (boolean)           True if my_array is indexed with [row][col] (it needs to be flipped)
+                                        False if my_array is indexed with [col][row] (it gets copied as is)
+        """
+        if (transpose)&(my_array.ndim>1):
+            self.__data=np.swapaxes(my_array,0,1)
+        else:
+            self.__data=my_array
         
+    @classmethod
+    def fromDimensions(cls, rows, columns):
+        data = np.zeros((columns,rows),dtype=int)
+        return cls(data, True)
+    
+    
+    def getPoint(self, x, y):
+        """
+        Goes to x,y location in PixelArray and gets color there.  Returns that color
+        """
+        return self.__data[x][y]
+
+    def filter(self, indices, axis:int):
+        """
+        This function will return a new PixelArray that is filtered with specific rows or columns based on an array of boolean values for each row/column
+
+        indices:    (np.array)  1D array with boolean values for each element in the axis.  True will keep the row, False will drop it
+        axis:       (int)       This indicates the axis by which to apply the indices boolean array.  value must be either 0 or 1.  0=Rows, 1=columns.
+        
+        returns a new PixelArray filtered based on boolean element-wise index
+        """
+        if np.all((indices==True)): # none of sprite is clipped
+            return self
+        elif np.all((indices==False)): # whole sprite is clipped
+            empty_array=np.array([])
+            return PixelArray(my_array=empty_array,transpose=False)
+        else: # some part of sprite is clipped
+            if axis==0: # indexing rows
+                this_arr_index =np.tile(indices,(self.getWidth(),1))
+                this_arr=self.__data[this_arr_index]
+                this_arr = this_arr.reshape(self.getWidth(),int(len(this_arr)/self.getWidth()))
+                if this_arr.ndim==1: this_arr=np.array([this_arr])
+                return PixelArray(my_array=this_arr,transpose=False)
+            elif axis==1: # indexing columns
+                this_arr_index = np.tile(indices,(self.getHeight(),1)).T
+                this_arr=self.__data[this_arr_index]
+                this_arr = this_arr.reshape(int(len(this_arr)/self.getHeight()),self.getHeight())
+                if this_arr.ndim==1: this_arr=np.array([this_arr])
+                return PixelArray(my_array=this_arr,transpose=False)
+            
+
+    def getWidth(self):
+        """
+        returns the width of the PixelArray
+        """
+        if self.__data.size>0:
+            return self.__data.shape[0]
+        else:
+            return 0
+
+
+    def getHeight(self):
+        """
+        returns the height of the PixelArray
+        """
+        if self.__data.size>0:
+            return self.__data.shape[1]
+        else:
+            return 0
+
+
+    def getSize(self):
+        return self.__data.size
+
+    def setPoint(self, x:int, y:int, value:int):
+        """
+        Goes to x,y location in array and sets element to value.  Value should be an integer from 0-16
+        """
+        self.__data[x][y]=int(value)
+
+    def fillArray(self, color_code):
+        self.__data.fill(color_code)
+
+    def getData(self):
+        return self.__data
+
+    def toString(self):
+        print(self.__data.T)
+
+
 class Screen:
     """
     Manages the Screen that we will draw on.  Pixels must all fall within the x,y coordinates of the defined screen space.
@@ -259,121 +378,7 @@ class Clipper:
         return (clipped_sprite, sprite_start_pos)
             
 
-class PixelArray:
-    """
-    This class creates an manages a raster of pixels.  Each pixel spot holds an encoded color as shown below:
-
-    0=black wool
-    1=green wool
-    2=blue wool
-    3=red wool
-    4=white wool  
-    5=Magenta wool
-    6=Brown wool
-    7=Purple wool
-    8=Cyan wool
-    9=Transparent/do not draw
-    10=Grey wool
-    11=Pink wool
-    12=Lime wool
-    13=Yellow wool
-    14=Light Blue wool
-    15=Orange wool
-    16=Light Grey wool
-    """
-
-
-    def __init__(self, my_array, transpose=True):
-        """
-        Creates a new PixelArray from an existing array
-
-        my_array:   (numpy.ndarray)     1D or 2D array representing the pixelspace
-        transpose:  (boolean)           True if my_array is indexed with [row][col] (it needs to be flipped)
-                                        False if my_array is indexed with [col][row] (it gets copied as is)
-        """
-        if (transpose)&(my_array.ndim>1):
-            self.__data=np.swapaxes(my_array,0,1)
-        else:
-            self.__data=my_array
-        
-    @classmethod
-    def fromDimensions(cls, rows, columns):
-        data = np.zeros((columns,rows),dtype=int)
-        return cls(data, True)
-    
-    
-    def getPoint(self, x, y):
-        """
-        Goes to x,y location in PixelArray and gets color there.  Returns that color
-        """
-        return self.__data[x][y]
-
-    def filter(self, indices, axis:int):
-        """
-        This function will return a new PixelArray that is filtered with specific rows or columns based on an array of boolean values for each row/column
-
-        indices:    (np.array)  1D array with boolean values for each element in the axis.  True will keep the row, False will drop it
-        axis:       (int)       This indicates the axis by which to apply the indices boolean array.  value must be either 0 or 1.  0=Rows, 1=columns.
-        
-        returns a new PixelArray filtered based on boolean element-wise index
-        """
-        if np.all((indices==True)): # none of sprite is clipped
-            return self
-        elif np.all((indices==False)): # whole sprite is clipped
-            empty_array=np.array([])
-            return PixelArray(my_array=empty_array,transpose=False)
-        else: # some part of sprite is clipped
-            if axis==0: # indexing rows
-                this_arr_index =np.tile(indices,(self.getWidth(),1))
-                this_arr=self.__data[this_arr_index]
-                this_arr = this_arr.reshape(self.getWidth(),int(len(this_arr)/self.getWidth()))
-                if this_arr.ndim==1: this_arr=np.array([this_arr])
-                return PixelArray(my_array=this_arr,transpose=False)
-            elif axis==1: # indexing columns
-                this_arr_index = np.tile(indices,(self.getHeight(),1)).T
-                this_arr=self.__data[this_arr_index]
-                this_arr = this_arr.reshape(int(len(this_arr)/self.getHeight()),self.getHeight())
-                if this_arr.ndim==1: this_arr=np.array([this_arr])
-                return PixelArray(my_array=this_arr,transpose=False)
-            
-
-    def getWidth(self):
-        """
-        returns the width of the PixelArray
-        """
-        if self.__data.size>0:
-            return self.__data.shape[0]
-        else:
-            return 0
-
-
-    def getHeight(self):
-        """
-        returns the height of the PixelArray
-        """
-        if self.__data.size>0:
-            return self.__data.shape[1]
-        else:
-            return 0
-
-
-    def getSize(self):
-        return self.__data.size
-
-    def setPoint(self, x:int, y:int, value:int):
-        """
-        Goes to x,y location in array and sets element to value.  Value should be an integer from 0-16
-        """
-        self.__data[x][y]=int(value)
-
-    def fillArray(self, color_code):
-        self.__data.fill(color_code)
-
-    def getData(self):
-        return self.__data
-
-    def toString(self):
-        print(self.__data.T)
+      
 
 class Painter:
     """
@@ -393,4 +398,9 @@ class Painter:
         """
         
         my_clipped_sprite, clipped_sprite_pos = self.__clipper.clipObjectWithScreenEdges(my_sprite, sprite_pos)
-        
+        self.__my_screen.drawObject(my_clipped_sprite, clipped_sprite_pos)
+
+    def fillCanvas(self, color:int):
+        self.__my_screen.fill(color)
+
+   
---
+++ tests/test_Painter.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+import pickle
+from mcpi.minecraft import Minecraft
+from pong import utility
+from pong.render import Screen
+from pong.render import PixelArray
+
+
+class TestPainter(unittest.TestCase):
+    def test_init(self):
+        pass
+
+
+    def test_paintSprite(self):
+        pass
+
+
+    def test_fillCanvas(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
