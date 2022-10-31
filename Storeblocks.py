222a81fdbe84ba729ebbfef54353852e70538a53
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-10-31 18:45:25 -0400

    On branch 5-add-input-class-and-objectpaddle  Your branch is up to date with 'origin/5-add-input-class-and-objectpaddle'.
     Changes to be committed:
    	new file:   Storeblocks.py
    	new file:   assets/paddle.pkl
    	new file:   assets/screen.pkl
    	modified:   main.py
    - Added new assets for paddle and screen

---
+++ Storeblocks.py
@@ -0,0 +1,50 @@
+
+from mcpi.minecraft import Minecraft
+from mcpi import vec3
+from mcpi import block
+import pickle
+import math
+import numpy as np
+from pong.render import Screen
+from pong import utility
+import time
+from mcpi_block_structure.blockstructure import BlockStructure
+
+server_ip, server_port = pickle.load(open( "server.pkl", "rb" ) )
+mc = Minecraft.create(server_ip,server_port)
+print(type(mc))
+
+# These vectors are all in world space
+top_left_screen_coord = utility.get_mcpi_vec_from_world_coords(39562, 106, 39958) # pixel display..  Display is facing west
+screen_nw_bot_corner = utility.get_mcpi_vec_from_world_coords(39562,74,39957) # Monitor(display) Bottom NW Corner
+p1_paddle_nw_bot_corner = utility.get_mcpi_vec_from_world_coords(39522, 78, 39954) # Player 1 paddle
+p2_paddle_nw_bot_corner = utility.get_mcpi_vec_from_world_coords(39522, 78, 39968) # player 2 paddle
+
+
+
+# paddle storage
+#my_paddle = BlockStructure(mc)
+#start_vec = my_paddle.get_mcpi_vec_from_world_coords(39522, 78, 39968)
+#end_vec = my_paddle.get_mcpi_vec_from_world_coords(39538, 88, 39977)
+
+#my_paddle.get_structure(start_vec, end_vec)
+
+#my_paddle.write_to_file("my_paddle.pkl")
+
+#screen storage
+my_screen_object = BlockStructure(mc)
+start_vec = screen_nw_bot_corner
+end_vec = utility.get_mcpi_vec_from_world_coords(39563,107,39974)
+my_screen_object.get_structure(start_vec, end_vec)
+my_screen_object.write_to_file("my_screen.pkl")
+
+#paddle = BlockStructure(mc)
+#paddle.read_from_file(filename="my_paddle.pkl")
+#p1_paddle_nw_bot_corner = vec3.Vec3(39522, 78, 39954) # Player 1 paddle
+#p1_paddle_nw_bot_corner = paddle.get_mcpi_vec_from_world_vec(p1_paddle_nw_bot_corner)
+
+#paddle.set_structure(p1_paddle_nw_bot_corner)
+
+screen = BlockStructure(mc)
+screen.read_from_file("my_screen.pkl")
+screen.set_structure(start_vec)
