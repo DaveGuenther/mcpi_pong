2a4354a24b43740134df5802e68bdd07498423ee
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-11-08 17:17:32 -0500

    On branch 8-create-cartesian-plane-for-drawing  Your branch is up to date with 'origin/8-create-cartesian-plane-for-drawing'.
     Changes to be committed:
    	renamed:    pong/cartesian_plane.py -> pong/cartesian_converter.py
    - Instead of calling the class a CartesianPlane, I'm calling it
      CartesianConverter

--- pong/cartesian_converter.py
+++ pong/cartesian_converter.py
@@ -13,12 +13,12 @@ class CartisianPlane:
         
         returns     tuple               2d vector representing caretsian coordinates
         """
-        cart_x = (self.screen_width/2)-((self.screen_width-1)/x)
-        cart_y = (self.screen_height/2)-((self.screen_height-1)/x)
+        cart_x=(vec[0]-(self.width/2))/(self.screen_width/self.screen_height)
+        cart_y=(vec[1]-(self.height/2))
         return (cart_x, cart_y)
 
     
-    def cartToScreen(x,y):
+    def cartToScreen(vec):
         """
         Takes a 2d vector with cartesian coordinates (top of visible plane is 1.0, bottom is -1.0, left and right are calculated based on the aspect ratio of the screen_width and screen_height), and converts those coordinates to Screen coordinates (top left is 0,0).
         
@@ -26,8 +26,8 @@ class CartisianPlane:
         
         returns     tuple               2d vector representing screen coordinates
         """
-        screen_x = 1/((-1*x+(self.screen_width/2))/(self.screen_width-1))  # Thanks Raju and Kimbra :)
-        screen_y = 1/((-1*y+(self.screen_height/2))/(self.screen_height-1))
+        screen_x=(self.width/2)+(vec[0]*(self.screen_width/self.screen_height))
+        screen_y=(screen.height/2)+vec[1]
         return (screen_x, screen_y)
 
 my_vec = (0,0)
@@ -35,10 +35,10 @@ screen_width=16
 screen_height=32
 
 
-cart_x = (screen_width/2)-((screen_width-1)-my_vec[0])
-cart_y = (screen_height/2)-((screen_height-1)-my_vec[1])
+cart_x=(my_vec[0]-(screen_width/2))/(screen_width/screen_height)
+cart_y=(my_vec[1]-(screen_height/2))
 print (cart_x, cart_y)
 
-screen_x = 1/((-1*my_vec[0]+(screen_width/2))/(screen_width-1))  # Thanks Raju and Kimbra :)
-screen_y = 1/((-1*my_vec[1]+(screen_height/2))/(screen_height-1))
+screen_x=(screen_width/2)+(my_vec[0]*(screen_width/screen_height))
+screen_y=(screen_height/2)+my_vec[1]
 print (screen_x, screen_y)
