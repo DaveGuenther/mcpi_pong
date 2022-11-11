7f7a4d014b1606e075211accd8d4b4e5ae3f2f59
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-11-11 18:12:46 -0500

    Completed unittests for MatrixTools  On branch 8-create-cartesian-plane-for-drawing  Your branch is up to date with 'origin/8-create-cartesian-plane-for-drawing'.
     Changes to be committed:
    	modified:   makefile
    	new file:   tests/test_MatrixTools.py

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 RENDERER_TESTS = tests/test_PixelArray.py tests/test_Color.py tests/test_Clipper.py tests/test_Screen.py tests/test_Painter.py
-MATH_TESTS = tests/test_CoordinateTools.py
+MATH_TESTS = tests/test_CoordinateTools.py tests/test_MatrixTools.py
 
 test: 
 	python -m unittest -v $(RENDERER_TESTS) $(MATH_TESTS)
---
+++ tests/test_MatrixTools.py
@@ -0,0 +1,79 @@
+import unittest
+import numpy as np
+import pickle
+import math
+from pong.matrix_tools import MatrixTools
+
+class TestMatrixTools(unittest.TestCase):
+    def testVectorLength(self):
+        my_vec = np.array([0,3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertTrue(my_vec_length==3)
+
+        my_vec = np.array([-3,3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertAlmostEqual(my_vec_length, 4.242640687)
+
+        my_vec = np.array([-3,0])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertTrue(my_vec_length==3)
+
+        my_vec = np.array([-3,-3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertAlmostEqual(my_vec_length, 4.242640687)       
+
+        my_vec = np.array([0,-3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertTrue(my_vec_length==3)
+
+        my_vec = np.array([3,-3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertAlmostEqual(my_vec_length, 4.242640687)  
+
+        my_vec = np.array([3,0])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertTrue(my_vec_length==3)
+
+        my_vec = np.array([3,3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        self.assertAlmostEqual(my_vec_length, 4.242640687)  
+
+    def testUnitVector(self):
+        my_vec = np.array([0,3])
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        my_vec = MatrixTools.getUnitVector(my_vec)
+        self.assertTrue((my_vec==np.array([0,1])).all())
+   
+
+    def testRotateVector(self):
+        #test 1
+        my_vec = np.array([0,3])
+
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        my_vec = MatrixTools.getUnitVector(my_vec)
+        my_vec = MatrixTools.rotateVector(-90.0, my_vec)
+        my_vec = my_vec*my_vec_length  
+
+        self.assertAlmostEqual(my_vec[0], 3.0)  
+        self.assertAlmostEqual(my_vec[1], 0) 
+
+        #test 2
+        my_vec = np.array([0,3])
+
+        my_vec_length = MatrixTools.getVectorLength(my_vec)
+        my_vec = MatrixTools.getUnitVector(my_vec)
+        my_vec = MatrixTools.rotateVector(0, my_vec)
+        my_vec = my_vec*my_vec_length  
+        print(round(my_vec[0],3))    
+        self.assertAlmostEqual(my_vec[1], 3.0)  
+        self.assertAlmostEqual(my_vec[0], 0)          
+
+def testGetOrthogonalForceVector(self):
+        my_vec = np.array([0.01,6.01])
+        my_force_vec = MatrixTools.getOrthogonalForceVector(my_vec, 1)
+        my_vec = my_vec+my_force_vec
+        my_vec = MatrixTools.getUnitVector(my_vec)
+        print(my_vec)
+
+if __name__ == '__main__':
+    unittest.main()
