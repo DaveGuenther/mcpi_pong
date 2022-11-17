b56fc956b67ce484b6086edbc20d60f52fc90a27
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-11-16 23:04:17 -0500

    Added Vector unittests.  Continue on Input Class  On branch 13-create-input-class  Your branch is up to date with 'origin/13-create-input-class'.
     Changes to be committed:
    	modified:   makefile
    	modified:   pong/vector.py
    	new file:   tests/test_Vector.py

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 RENDERER_TESTS = tests/test_PixelArray.py tests/test_Color.py tests/test_Clipper.py tests/test_Screen.py tests/test_Renderer.py
-MATH_TESTS = tests/test_CoordinateTools.py tests/test_MatrixTools.py
+MATH_TESTS = tests/test_CoordinateTools.py tests/test_MatrixTools.py tests/test_Vector.py
 GAME_TESTS = tests/test_GameObject.py
 
 test: 
--- pong/vector.py
+++ pong/vector.py
@@ -1,9 +1,9 @@
 from mcpi import vec3
 import numpy as np
-import constant
+from . import constant
 
 class Vector:
-    world_to_mcpi_offset = constant.Constant(vec3.Vec3(-32,66,128))
+    WORLD_TO_MCPI_OFFSET = constant.Constant(vec3.Vec3(-32,66,128))
 
     def __init__(self, world_vec3, mcpi_vec3):
         self.__world_vec=world_vec3
@@ -33,35 +33,35 @@ class Vector:
 
     def __mcpi_to_world(self, mcpi_vec):
         world_vec = vec3.Vec3(
-            mcpi_vec.x+Vector.world_to_mcpi_offset.get().x,
-            mcpi_vec.y+Vector.world_to_mcpi_offset.get().y,
-            mcpi_vec.z+Vector.world_to_mcpi_offset.get().z
+            mcpi_vec.x+Vector.WORLD_TO_MCPI_OFFSET.get().x,
+            mcpi_vec.y+Vector.WORLD_TO_MCPI_OFFSET.get().y,
+            mcpi_vec.z+Vector.WORLD_TO_MCPI_OFFSET.get().z
         )
 
         return world_vec    
 
     def __world_to_mcpi(self, world_vec):
         mcpi_vec = vec3.Vec3(
-            world_vec.x-Vector.world_to_mcpi_offset.get().x,
-            world_vec.y-Vector.world_to_mcpi_offset.get().y,
-            world_vec.z-Vector.world_to_mcpi_offset.get().z
+            world_vec.x-Vector.WORLD_TO_MCPI_OFFSET.get().x,
+            world_vec.y-Vector.WORLD_TO_MCPI_OFFSET.get().y,
+            world_vec.z-Vector.WORLD_TO_MCPI_OFFSET.get().z
         )
         return mcpi_vec
     
     def set_MCWorld_XYZ(self, x,y,z):
         self.__world_vec = vec3.Vec3(x,y,z)
-        self.__mcpi_vec=Vector.__world_to_mcpi(self.__world_vec)
+        self.__mcpi_vec=self.__world_to_mcpi(self.__world_vec)
 
     def set_mcpi_XYZ(self, x,y,z):
         self.__mcpi_vec = vec3.Vec3(x,y,z)
-        self.__world_vec=Vector.__mcpi_to_world(self.__mcpi_vec)
+        self.__world_vec=self.__mcpi_to_world(self.__mcpi_vec)
 
-    def set_MCWorld_XYZ(self, world_vec):
-        self.__mcpi_vec=Vector.__world_to_mcpi(world_vec)
+    def set_MCWorld_Vec(self, world_vec):
+        self.__mcpi_vec=self.__world_to_mcpi(world_vec)
         self.__world_to_mcpi=world_vec
 
     def set_mcpi_Vec(self, mcpi_vec):
-        self.__world_vec=Vector.__mcpi_to_world(mcpi_vec)
+        self.__world_vec=self.__mcpi_to_world(mcpi_vec)
         self.__mcpi_vec=mcpi_vec
 
     def get_mcpiVec(self):
@@ -72,17 +72,3 @@ class Vector:
 
 
 
-print(Vector.world_to_mcpi_offset)
-my_world_vec = Vector.from_mcpi_XYZ(1,1,1)
-my_mcpi_vec = Vector.from_MCWorld_XYZ(
-    my_world_vec.get_MCWorld_Vec().x, 
-    my_world_vec.get_MCWorld_Vec().y, 
-    my_world_vec.get_MCWorld_Vec().z)
-my_vec = vec3.Vec3(1,1,1)
-my_world_vec2 = Vector.from_mcpi_Vec(my_vec)
-my_mcpi_vec2 = Vector.from_MCWorld_Vec(
-    vec3.Vec3(
-        my_world_vec.get_MCWorld_Vec().x, 
-        my_world_vec.get_MCWorld_Vec().y, 
-        my_world_vec.get_MCWorld_Vec().z))
-print("All Done")
