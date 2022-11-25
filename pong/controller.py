4be9e45700f5ebac124fccea8e065c2dd6b4915c
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-11-25 11:27:30 -0500

    Still Going

--- pong/controller.py
+++ pong/controller.py
@@ -2,5 +2,10 @@ from . import input
 from ..mcpi_block_structure import blockstructure
 
 class Controller:
-    def __init__(self):
-        pass
+    def __init__(self, mc_blockstructure:blockstructure, block_structure_start_pos:MCVector, input_object:input.InputInterface):
+        """
+        Associates a blockstructure with an InputInterface object
+        mc_block_structure              blockstructure          This is a blockstructure object created from the minecraft world.
+        block_structure_start_pos       MCVector                MCVector representation of an x,y,z coordinate of one corner of the cuboid to place in the MC world.  This vector must be at the base of the structure on the NW corner.  If you stand at the corner of the structure and look away from it, and find that you are facing just between north and west, you're on the right corner.
+        
+        """
