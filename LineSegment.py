60842d5e0bbd5ec20d6d4fe4a79c453e6bd10c40
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-12-06 23:40:30 -0500

    Worked on Vertical and Horizontal 'edge' cases in Line Segment class. Ready to write unittests

--- LineSegment.py
+++ LineSegment.py
@@ -4,8 +4,37 @@ import numpy as np
 A = LineSegment(np.array([4,2]),np.array([2,4]))
 B = LineSegment(np.array([1,2]),np.array([4,4]))
 
-x = A.xInterceptWith(B)
+x = A.interceptWith(B)
 print(x)
-if x:
-    y=A.getYfromX(x)
-    print(y)
+
+
+
+
+A = LineSegment(np.array([-2,4]),np.array([-1,2]))
+B = LineSegment(np.array([-3,3]),np.array([2,3]))
+
+x = A.interceptWith(B)
+print(x)
+
+
+A = LineSegment(np.array([-2,2]),np.array([-2,-1])) # Vertical
+B = LineSegment(np.array([-1,1]),np.array([-3,1])) # Horizontal
+
+x = A.interceptWith(B)
+print(x)
+
+
+A = LineSegment(np.array([-1,1]),np.array([-3,1])) # Vertical
+B = LineSegment(np.array([-2,2]),np.array([-2,-1])) # Horizontal
+
+x = A.interceptWith(B)
+print(x)
+
+A = LineSegment(np.array([-2,2]),np.array([-2,-1])) # Vertical
+B = LineSegment(np.array([-1,1]),np.array([1,1])) # Horizontal
+
+x = A.interceptWith(B)
+print(x)
+
+x = B.interceptWith(A)
+print(x)
