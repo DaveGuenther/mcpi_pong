962d8d8ef5d6c82af93e802649f8fbe3e6a76074
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-12-15 20:14:13 -0500

    Working through edge case of colision where edge segment is vertical

--- Collision.py
+++ Collision.py
@@ -15,15 +15,25 @@ edge_screen=LineSegment(np.array([-1,2]),np.array([-1,-2]))
 intercept = np.array([-1,1])
 
 #subtract intercept from both secments
-origin_ball=LineSegment(ball_screen.getPoints()[0]-intercept, ball_screen.getPoints()[1]-intercept)
+origin_ball=LineSegment(ball_screen.getPoints()[0]-intercept, ball_screen.getPoints()[1]-intercept, directional=True)
 origin_edge=LineSegment(edge_screen.getPoints()[0]-intercept, edge_screen.getPoints()[1]-intercept)
 
 if edge_screen.getSlope()=='vertical':
     #rotate ball by -90 deg
     ball_vec_end_in_edge_space = MatrixTools.rotateVector(-90, origin_ball.getPoints()[1])
-    print(ball_vec_end_in_edge_space)
+    
+    #flip y coord of ball endpoint
+    ball_vec_end_in_edge_space[1]=ball_vec_end_in_edge_space[1]*-1
+
+    #rotate ball by 90 deg
+    new_ball_end_in_screen_space = MatrixTools.rotateVector(90, ball_vec_end_in_edge_space)
+
 elif edge_screen.getSlope()==0:
     pass
 else:
     #slope is either positive or negative but not vertical or horizontal
     pass
+
+#add intercept to ball_vec
+reflected_ball_vec = LineSegment(intercept, new_ball_end_in_screen_space,directional=True)
+print('hello')
